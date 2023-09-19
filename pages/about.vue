160c9ae08918979d70a88f7b7f1d1fd1c35317cd
罗天旭 <1265062404@qq.com>
2023-09-19 14:33:30 +0800

    feat(custom): 格式校验

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>234</div>
+	<div>234</div>
 </template>
 
 <script lang="ts" setup></script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="main">
-    <div class="title">hello world</div>
-  </div>
+	<div class="main">
+		<div class="title">hello world</div>
+	</div>
 </template>
 
 <script lang="ts" setup></script>
 
 <style lang="scss" scoped>
 .main {
-  .title {
-    color: red;
-  }
+	.title {
+		color: red;
+	}
 }
 </style>
